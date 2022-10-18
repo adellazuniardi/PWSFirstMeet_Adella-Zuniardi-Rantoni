046dc7dc910b485f96fd64d0c6919d096b9727d5
adellazuniardi <96398704+adellazuniardi@users.noreply.github.com>
2022-10-18 20:10:44 +0700

    step1 - membuat project baru

---
+++ src/test/java/TugasCemilan1/PWS/cemilan1/PWS/ApplicationTests.java
@@ -0,0 +1,13 @@
+package TugasCemilan1.PWS.cemilan1.PWS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
