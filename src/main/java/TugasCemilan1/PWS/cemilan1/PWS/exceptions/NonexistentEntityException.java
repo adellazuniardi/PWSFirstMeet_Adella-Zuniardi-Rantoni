955d32ec1e7cbf40bfadb0df52571c528ea5f315
adellazuniardi <96398704+adellazuniardi@users.noreply.github.com>
2022-10-18 20:16:37 +0700

    step3 - add jpa controller

---
+++ src/main/java/TugasCemilan1/PWS/cemilan1/PWS/exceptions/NonexistentEntityException.java
@@ -0,0 +1,10 @@
+package TugasCemilan1.PWS.cemilan1.PWS.exceptions;
+
+public class NonexistentEntityException extends Exception {
+    public NonexistentEntityException(String message, Throwable cause) {
+        super(message, cause);
+    }
+    public NonexistentEntityException(String message) {
+        super(message);
+    }
+}
